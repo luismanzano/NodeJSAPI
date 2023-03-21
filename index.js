e3621536516a2afee0c8659f992f5760a50b8db1
Luis Fernando Manzano Alvarez <luis.m@rivka.mx>
2023-03-21 15:49:07 -0400

    added support for cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 let notes = [
@@ -72,7 +74,7 @@ app.post('/api/notes', (req, res) => {
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
